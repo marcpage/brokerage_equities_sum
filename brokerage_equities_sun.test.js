db31cfc833293a58ac062e107b54ac7abbae36d2
Marc Page <MarcAllenPage@gmail.com>
2023-11-05 18:27:03 -0600

    Adding larger test

--- brokerage_equities_sun.test.js
+++ brokerage_equities_sun.test.js
@@ -1,5 +1,11 @@
 const code_under_test = require("./brokerage_equities_sum");
 
-test("Test parsing empty input", () => {
-    expect(code_under_test.parse_and_add("", {})).toBe("\n");
+test("Test parsing empty input, no symbols", () => {
+    expect(code_under_test.parse_and_add("", {}))
+        .toBe("\n");
+});
+
+test("Test parsing empty input, one symbol", () => {
+    expect(code_under_test.parse_and_add("", {AAPL: 55.00}))
+        .toBe("\n# unseen: AAPL\n# Total value = $55.00\n\n");
 });
