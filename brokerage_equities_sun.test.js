c7977d588983eeff9450e0262170eaf28ec92cd6
Marc Page <MarcAllenPage@gmail.com>
2023-11-05 19:07:40 -0600

    adding some more test cases

--- brokerage_equities_sun.test.js
+++ brokerage_equities_sun.test.js
@@ -10,7 +10,23 @@ test("Test parsing empty input, one symbol", () => {
         .toBe("\n# unseen: AAPL\n# Total value = $55.00\n\n");
 });
 
-test("Test parsing empty input, one symbol", () => {
+test("Test parsing one input, one symbol", () => {
     expect(code_under_test.parse_and_add("AAPL\n", {AAPL: 55.00}))
         .toBe("AAPL\n# Total value = $55.00\n\n\n");
+});
+
+test("Test parsing two inputs, one symbol", () => {
+    expect(code_under_test.parse_and_add("AAPL MSFT\n", {AAPL: 55.00}))
+        .toBe("AAPL MSFT\n# Symbol MSFT was not found\n# Total value = $55.00\n\n\n");
+});
+
+test("Test parsing two inputs, two symbols", () => {
+    expect(code_under_test.parse_and_add("AAPL MSFT\n", {AAPL: 55.00, MSFT: 110.01}))
+        .toBe("AAPL MSFT\n# Total value = $165.01\n\n\n");
+});
+
+test("Test parsing 2x2 inputs, 4 symbols", () => {
+    expect(code_under_test.parse_and_add("AAPL MSFT\nVTI VYM\n", 
+        {AAPL: 55.00, MSFT: 110.01, VTI: 12.34, VYM: 43.21}))
+        .toBe("AAPL MSFT\n# Total value = $165.01\n\nVTI VYM\n# Total value = $55.55\n\n\n");
 });
